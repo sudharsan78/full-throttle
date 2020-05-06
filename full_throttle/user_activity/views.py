53bc7e9a955ab2f675bc735f5ec513115c8685ba
Sudharsan <sudharsan@ad.infosys.com>
2020-05-06 16:06:06 +0530

    Matched json response to expected

--- full_throttle/user_activity/views.py
+++ full_throttle/user_activity/views.py
@@ -1,4 +1,5 @@
 from rest_framework.generics import ListAPIView
+from rest_framework.response import Response
 
 from .models import User
 from .serializers import UserActivitySerializer
@@ -8,3 +9,9 @@ class ActivityList(ListAPIView):
     queryset = User.objects.only('user_id', 'time_zone').filter(is_superuser=False).prefetch_related('useractivity_set')
     serializer_class = UserActivitySerializer
 
+    def list(self, request, *args, **kwargs):
+        response = super().list(request, args, kwargs)
+        data_to_return = {'ok': True, 'members': response.data}
+        if response.status_code != 200:
+            data_to_return = {'ok': False, 'result': response.data}
+        return Response(data=data_to_return)
